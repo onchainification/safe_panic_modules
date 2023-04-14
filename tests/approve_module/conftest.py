572c1c03eedda496b70adf55b946e7fc6386778b
Petrovska <petrovska@badger.com>
2023-04-14 21:30:54 +0900

    test: hook test with oz deps and deploy fake erc20

--- tests/approve_module/conftest.py
+++ tests/approve_module/conftest.py
@@ -1,11 +1,28 @@
 import pytest
-from brownie import accounts, interface, ApprovalModule
+from brownie import ApprovalModule
+
+
+OZ_DEPS = "OpenZeppelin/openzeppelin-contracts@4.5.0"
 
 
 @pytest.fixture
-def deployer():
+def deployer(accounts):
     return accounts[0]
 
 
+@pytest.fixture()
+def rug_puller(accounts):
+    return accounts[1]
+
+
+@pytest.fixture()
 def module(deployer):
     module = ApprovalModule.deploy({"from": deployer})
+    return module
+
+
+@pytest.fixture()
+def token(pm, deployer):
+    oz = pm(OZ_DEPS)
+    fake_token = oz.ERC20.deploy("rugPullToken", "NGMI", {"from": deployer})
+    return fake_token
