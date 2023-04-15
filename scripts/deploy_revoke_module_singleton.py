fd6fab578e93c14de85efd4e9cbf004aa74e02e6
Petrovska <petrovska@badger.com>
2023-04-15 16:44:44 +0900

    feat: add script for singleton verification for later factory deploy contract verification process on matching bytcode

---
+++ scripts/deploy_revoke_module_singleton.py
@@ -0,0 +1,8 @@
+from brownie import RevokeModule, accounts
+
+
+def main(deployer_label=None):
+    deployer = accounts.load(deployer_label)
+
+    goerli_msig = "0x206C89813cbDE8E14582Ff94F3F1A1728C39a300"
+    return RevokeModule.deploy(goerli_msig, {"from": deployer}, publish_source=True)
